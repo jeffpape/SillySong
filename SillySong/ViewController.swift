097596e71a176dafb2b24c21da800d1d1b2e46f3
Jeff Pape <jeff.pape@gmail.com>
2018-01-30 23:01:28 -0600

    Initial Commit

---
+++ SillySong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SillySong
+//
+//  Created by Jeffrey Pape on 18-1-30.
+//  Copyright © 2018 Pape Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
